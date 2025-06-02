1d6ed537fdb768b77655bf2a39a3362e96e23c2a
HenriqueR199 <henrireis1@gmail.com>
2025-06-02 14:02:09 +0000

    modifica a frase

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home () {
-    return <h1>iaee mano, esse trem aqui é dificil em kkk. mas com dedicação vai dar tudo certo</h1>
+    return <h1>iaee mano, esse trem aqui é dificil. Mas com dedicação e perceverança você vai conseguir e vai dar tudo certo, não desista.</h1>
 }
 
 export default Home;
