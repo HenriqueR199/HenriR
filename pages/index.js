4236ee09d68ab5847b81bb7488b9c316caccbb9c
HenriqueR199 <henrireis1@gmail.com>
2025-06-04 14:13:42 +0000

    adiciona script `lint:check` e´`lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,10 @@
-function Home () {
-    return <h1>iaee mano, esse trem aqui é dificil. Mas com dedicação e perceverança você vai conseguir e vai dar tudo certo, não desista.</h1>
+function Home() {
+  return (
+    <h1>
+      iaee mano, esse trem aqui é dificil. Mas com dedicação e perceverança você
+      vai conseguir e vai dar tudo certo, não desista.
+    </h1>
+  );
 }
 
 export default Home;
